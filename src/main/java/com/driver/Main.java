c09590ee32caf109a71f8ec3c66fce25e9adbdf9
Your Name <Vijay>
2023-01-16 18:25:35 +0530

    Solution 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] args){
+      RWOnly obj=new RWOnly();
+      obj.setter("Vijay");
+      System.out.println(obj.getter());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void setter(String name){
+        this.name=name;
+    }
+    public String getter(){
+        return name;
+    }
 }
